8e5b7450d871a3932ee675c4396643f54c9dcc28
GinaTung <88545191+GinaTung@users.noreply.github.com>
2025-10-04 00:33:10 +0800

    環境初步建立和新增會員API

---
+++ backend_southernTravelWebsite2025/DTOs/MemberDtos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backend_southernTravelWebsite2025.DTOs
+{
+    // 建立會員用
+    public class MemberCreateDto
+    {
+        public string Name { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Password { get; set; } = ""; // 前端傳進來的原始密碼
+        public string PhoneNumber { get; set; } = "";
+        public DateTime Birthday { get; set; }
+    }
+
+    // 回傳給前端用
+    public class MemberResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string PhoneNumber { get; set; } = "";
+        public DateTime Birthday { get; set; }
+    }
+}
