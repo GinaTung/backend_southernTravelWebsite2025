330e8854234c37eff326e3b71217964cd892a72b
GinaTung <88545191+GinaTung@users.noreply.github.com>
2025-10-03 23:36:50 +0800

    加入專案檔案。

---
+++ backend_southernTravelWebsite2025/Models/Entities/Member.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backend_southernTravelWebsite2025.Models.Entities
+{
+    //資料模型層
+    public class Member
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime Birthday { get; set; }
+        public string? ProfileImage { get; set; }
+        public string? Notes { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } =DateTime.Now;
+    }
+}
